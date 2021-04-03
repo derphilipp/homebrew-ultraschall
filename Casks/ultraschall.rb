013c06b700a73541a02eca577317187fcef7a005
Philipp <mail@philipp-weissmann.de>
2021-04-03 19:52:50 +0200

    Adds dependency

--- Casks/ultraschall.rb
+++ Casks/ultraschall.rb
@@ -2,12 +2,15 @@ cask "ultraschall" do
   version "5.0.3"
   sha256 "3a39245100bb6deb9f4bae1ee339911b6394679b0ab61673e6a3febe6299429a"
 
+  depends_on cask: "ultraschall-reaper"
+
   url "https://files.rstockm.de/index.php/s/kcJKWLegjYadkiE/download", verified: "https://files.rstockm.de"
   name "Ultraschall"
   desc "Ultraschall High End Podcasting"
   homepage "https://ultraschall.fm/"
 
-  pkg "Ultraschall-#{version}.pkg"
+
+  pkg "Ultraschall-#{version}.pkg", allow_untrusted: true
 
   uninstall pkgutil: [
     "fm.ultraschall.studiolink",
